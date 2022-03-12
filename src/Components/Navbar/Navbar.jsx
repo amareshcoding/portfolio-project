b0a32e046244329ebecd573f0e11f7c35d8264fb
amareshcoding <amaresh.acb@gmail.com>
2022-03-12 17:36:38 +0530

    navbar

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,15 @@
+import './Navbar.css'
+
+export const Navbar =()=>{
+     return(
+          <div className="Navbar">
+               <div className='Navbar-div-1'>
+                    <h5 className='Navbar-h5'>HOME</h5>
+               </div>
+               <div className='Navbar-div-2'>
+                    <h5 className='Navbar-h5'>ABOUT</h5>
+                    <h5 className='Navbar-h5'>RESUME</h5>
+               </div>
+          </div>
+     )
+}
--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -1,7 +1,11 @@
 import './Portfolio.css'
+import { Navbar } from './Navbar/Navbar'
+
+
 
 export const Portfolio=()=>{
      return(<div className="Portfolio">
+          <Navbar/>
           <h1>Hello Amaresh</h1>
      </div>)
 }
