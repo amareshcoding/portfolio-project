41b9b187ee562f303f6bc21e1cf3260d03ea9feb
amareshcoding <amaresh.acb@gmail.com>
2022-03-12 18:20:52 +0530

    test push

---
+++ src/Components/About/About.jsx
@@ -0,0 +1,17 @@
+
+
+export const About=()=>{
+     return(
+          <div className="About">
+               <div>
+                    <h2>Hi, I am Amresh Chandra Barik</h2>
+                    <h3>FULL STACK DEVELOPER</h3>
+               </div>
+               <div>
+                    <div>
+                         <img src="" alt="" />
+                    </div>
+               </div>
+          </div>
+     )
+}
